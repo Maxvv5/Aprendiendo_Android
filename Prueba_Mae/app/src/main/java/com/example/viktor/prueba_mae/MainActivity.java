f283972009c053294bacf9f6e27386a849c8914d
Victor Rojas Barboza <victor.rojas98@gmail.com>
2016-05-19 11:31:50 -0600

    linea de comentario
    Mae le agregue una linea de comentario

---
+++ Prueba_Mae/app/src/main/java/com/example/viktor/prueba_mae/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.viktor.prueba_mae;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //MAE FIJESE SI UD PUEDE AGREGAR UNA LINEA DE COMENTARIO
+    }
+}
